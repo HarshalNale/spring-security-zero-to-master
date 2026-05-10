5b8d27979976eea048a172fd447b56ece0401ec8
HarshalNale <nharshal77@gmail.com>
2026-05-10 22:21:50 +0530

    initial commit of spring security

---
+++ springsecuritysection1/src/main/java/com/spring/security/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.spring.security.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    private static final Logger log = LoggerFactory.getLogger(WelcomeController.class);
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping
+    public String message() {
+        log.info(message);
+        return message;
+    }
+}
